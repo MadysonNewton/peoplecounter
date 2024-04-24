e2d0f872e3994e46aef68a2595c842c7f9a43f27
Madyson Newton <mnewton.prsvr@gmail.com>
2024-04-24 17:58:07 -0500

    added notes

--- note.js
+++ note.js
@@ -1,8 +1,23 @@
-let welcomeEl = document.getElementById("welcome-el")
-let name = " Mady Newton"
-let greeting = "Welcome back,"
+// let welcomeEl = document.getElementById("welcome-el")
+// let name = " Mady Newton"
+// let greeting = "Welcome back,"
 
-welcomeEl.innerText = greeting + name 
+// welcomeEl.innerText = greeting + name 
 
 
-welcomeEl.innerText += " 🙌"
+// welcomeEl.innerText += " 🙌"
+
+
+// let firstName = "Mady " 
+// let lastName = "Newton" 
+// let fullName = firstName + lastName
+// console.log(fullName)
+
+
+
+let name = "Linda"
+let greeting = "Hi there "
+
+function greetLinda() {
+    console.log(greeting + ",  linda + !")
+}
